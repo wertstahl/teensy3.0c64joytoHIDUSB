a0bb67332bd8602495aeedeba66cc4684707128d
Wertstahl <seedpeer@fahr-zur-hoelle.org>
2023-10-21 01:54:55 +0200

    Updated

---
+++ information/HID.cpp
@@ -0,0 +1,2 @@
+[InternetShortcut]
+URL=https://https://android.googlesource.com/platform/external/arduino-ide/+/f876b2abdebd02acfa4ba21e607327be4f9668d4/hardware/arduino/sam/cores/arduino/USB/HID.cpp
